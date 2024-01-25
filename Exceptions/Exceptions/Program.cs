1e1ec3201ed4e4d760239f2559a8147cacf6c526
MadMoth125 <madmoth125@gmail.com>
2024-01-25 09:36:19 -0600

    Create Program.cs

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿using Exceptions.Classes;
+using Exceptions.Interfaces;
+
+namespace Exceptions
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+			// List	of code	blocks
+			var	codeBlocks = new List<ICodeBlock>
+			{
+				new	NullReferenceExceptionBlock(),
+				new	DivideByZeroExceptionBlock(),
+				new	FileNotFoundExceptionBlock(),
+				new OddNumberExceptionBlock(),
+				// Add more	blocks if needed
+			};
+
+			// Execute code	blocks
+			foreach	(var codeBlock in codeBlocks)
+			{
+				codeBlock.Execute();
+
+				// Wait 1 second before executing the next block
+				await Task.Delay(1000);
+
+				// Add blank lines between each block's output
+				Console.WriteLine("\n");
+			}
+
+			// Wait for user input before exiting
+			Console.ReadKey();
+		}
+	}
+}
